74b38cc996d4866f9c29efb9bd1a5066cac3f468
Ella789 <eleung8@cps.edu>
2025-01-16 20:04:15 -0600

    Create bottleofmilksong.py

---
+++ 99 Bottles of milk/bottleofmilksong.py
@@ -0,0 +1,14 @@
+#Ella Leung
+#Oct 24, 2024
+#Init
+#Functions
+def bottles():
+    milk = 99
+    print(str(milk) + " bottles of milk on the wall " + str(milk) + " bottles of milk on the wall " + " take one done pass it around ")
+    a = 1
+    for i in range(99):
+        print(str(milk) + " bottles of milk on the wall " + str(milk) + " bottles of milk on the wall " + " take one done pass it around ")
+        milk = milk - 1
+    print(str(a) + " bottle of milk on the wall " + str(a) + " bottle of milk on the wall " + " take it done pass it around " + " No more bottles of milk on the wall " + " Boo Hoo! ")
+#Main
+bottles()
