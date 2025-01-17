b212f6032a5e980c374433fb89eb21165ad6b58a
Ella789 <eleung8@cps.edu>
2025-01-16 20:01:31 -0600

    Create madlibs.py

---
+++ MadLibs/madlibs.py
@@ -0,0 +1,12 @@
+#Ella
+#MadLibs 
+#Dec 9, 2024
+def storyline():
+    noun = input("Please enter a noun:")
+    pronoun = input ("Please enter a possesive pronoun:")
+    place = input("Please enter a place:")
+    name = input("Please enter a name:")
+    verb = input("Please enter a verb:")
+    print( "Today " + noun + " took a run in the " + place + pronoun + " loves to get brunch with " + name + " after her run " + " but today she saw a bid trying to eat her food so she had to " + verb)
+#Main
+storyline()
